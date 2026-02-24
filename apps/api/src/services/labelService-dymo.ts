b2409bb0c5f4c067bf86996729728fe0e119aefa
Stewart Bennell <stewart@bennell.me>
2026-02-25 09:43:56 +1100

    Update labelService-dymo.ts

--- apps/api/src/services/labelService-dymo.ts
+++ apps/api/src/services/labelService-dymo.ts
@@ -262,7 +262,7 @@ export async function createLabelPDF(
     const orgX = (LABEL_WIDTH_PT - orgWidth) / 2;
     page.drawText(orgText, {
       x: orgX,
-      y: 1,
+      y: 4,
       size: orgFontSize,
       font: boldFont,
       color: rgb(0, 0, 0),
